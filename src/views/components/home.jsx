0b2610980f89eb9764ee571d16e2ce54bcd9334f
Natyguimaraes <santosnath33@gmail.com>
2024-09-27 21:03:55 -0300

    a

--- src/views/components/home.jsx
+++ src/views/components/home.jsx
@@ -4,12 +4,30 @@ import FooterMenu from './FooterMenu';
 function Home(){
 
     return(
-        <div>
-            <h1> Hello </h1>
+
+        <div className="container_pai">
+            <div className="container_home">
+            <h1> Projetos </h1>
+                <div className="container_projetos"> 
+                    <img src="./imgyou.png"/>
+                    <img src="./imgyou.png"/>
+                    <img src="./imgyou.png"/>
+                    <img src="./imgyou.png"/>
+                    <img src="./imgyou.png"/>
+                </div>
+                <h1> Pessoas </h1>
+                <div className="container_usuarios">
+                    <img src="./imgperfil.jpg"/>
+                    <img src="./imgperfil.jpg"/>
+                    <img src="./imgperfil.jpg"/>
+                    <img src="./imgperfil.jpg"/>
+                    <img src="./imgperfil.jpg"/>
+                </div>
+            </div>
 
             <FooterMenu />
-
         </div>
+
     )
 }
 
